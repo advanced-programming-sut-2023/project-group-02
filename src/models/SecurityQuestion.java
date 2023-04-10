7c30a5e8c25daa83546f583ea66a265950cf1535
Hosein Khansari <hrk.x11@gmail.com>
2023-04-10 15:32:01 +0330

    add User and SecurityQuestion models

---
+++ src/models/SecurityQuestion.java
@@ -0,0 +1,7 @@
+package models;
+
+public enum SecurityQuestion {
+  FATHERS_NAME,
+  PET_NAME,
+  MOTHERS_LAST_NAME;
+}
---
+++ src/models/User.java
@@ -0,0 +1,74 @@
+package models;
+
+public class User {
+  private String username, password, nickname, email, slogan;
+  private SecurityQuestion securityQuestion;
+  private String securityAnswer;
+
+  public User(String username, String password, String nickname, String email, String slogan,
+      SecurityQuestion securityQuestion, String securityAnswer) {
+    this.username = username;
+    this.password = password;
+    this.nickname = nickname;
+    this.email = email;
+    this.slogan = slogan;
+    this.securityQuestion = securityQuestion;
+    this.securityAnswer = securityAnswer;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getSlogan() {
+    return slogan;
+  }
+
+  public SecurityQuestion getSecurityQuestion() {
+    return securityQuestion;
+  }
+
+  public String getSecurityAnswer() {
+    return securityAnswer;
+  }
+
+  public void setSecurityQuestion(SecurityQuestion securityQuestion) {
+    this.securityQuestion = securityQuestion;
+  }
+
+  public void setSecurityAnswer(String securityAnswer) {
+    this.securityAnswer = securityAnswer;
+  }
+
+  public void setSlogan(String slogan) {
+    this.slogan = slogan;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setNickname(String nickname) {
+    this.nickname = nickname;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+}
