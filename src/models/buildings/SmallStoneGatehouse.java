8a07c5b8420466706ab30ff9a87261e9530f0fd8
Hosein Khansari <hrk.x11@gmail.com>
2023-04-10 16:55:48 +0330

    fix SmallStoneGatehouse name + initialize name and hitpoint

--- src/models/buildings/SmallStoneGatehouse.java
+++ src/models/buildings/SmallStoneGatehouse.java
@@ -5,7 +5,10 @@ import java.util.ArrayList;
 import models.BuildingType;
 import models.MaterialInstance;
 
-public class SmallStoneGate {
+public class SmallStoneGatehouse {
   public static final BuildingType type = BuildingType.CASTLE_BUILDINGS;
   public static final ArrayList<MaterialInstance> cost = new ArrayList<>();
+
+  public final String name = "Small stone gatehouse";
+  public final int hitpoint = 1; // TODO ???
 }
