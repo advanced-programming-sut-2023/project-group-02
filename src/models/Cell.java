51ce5e9d5dea4fd5578ae390fe641cb64d2accc7
Hosein Khansari <hrk.x11@gmail.com>
2023-04-11 15:49:17 +0330

    Add some methods to Cell

--- src/models/Cell.java
+++ src/models/Cell.java
@@ -1,7 +1,34 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Cell {
     private Texture texture;
     private Building building;
+    private ArrayList<Unit> units;
 
+    public Cell(Texture texture) {
+        this.texture = texture;
+        this.units = new ArrayList<>();
+    }
+
+    Texture getTexture() {
+        return texture;
+    }
+
+    void setTexture(Texture texture) {
+        this.texture = texture;
+    }
+
+    Building getBuilding() {
+        return building;
+    }
+
+    boolean isOccupied() {
+        return building != null;
+    }
+
+    ArrayList<Unit> getUnits() {
+        return units;
+    }
 }
