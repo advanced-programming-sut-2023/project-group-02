af6ee0d53f780cb117e06fc55a211249663d5bf3
Hosein Khansari <hrk.x11@gmail.com>
2023-04-11 15:50:43 +0330

    Change map to use arrays

--- src/models/Map.java
+++ src/models/Map.java
@@ -1,15 +1,13 @@
 package models;
 
-import java.util.ArrayList;
-
 public class Map {
   public final int width;
   public final int height;
-  private final ArrayList<ArrayList<Cell>> map;
+  private final Cell[][] map;
 
   public Map(int width, int height) {
     this.width = width;
     this.height = height;
-    this.map = new ArrayList<ArrayList<Cell>>();
+    this.map = new Cell[width][height];
   }
 }
