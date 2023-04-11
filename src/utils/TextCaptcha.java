d8242d4069bc3adf5d488abcec3817d1d7fc84d0
Hosein Khansari <hrk.x11@gmail.com>
2023-04-11 15:59:59 +0330

    Add TextCaptcha util (to be implemented)

---
+++ src/utils/TextCaptcha.java
@@ -0,0 +1,18 @@
+package utils;
+
+public class TextCaptcha {
+  private final String answer;
+
+  public TextCaptcha(String answer) {
+    this.answer = answer;
+  }
+
+  public boolean checkAnswer(String answer) {
+    return this.answer.equals(answer);
+  }
+
+  public String getImage() {
+    // TODO
+    return "";
+  }
+}
