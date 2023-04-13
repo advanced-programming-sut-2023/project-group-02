b9e0ecb04e5651da73f53985e226a6d03e6654ac
Hosein Khansari <hrk.x11@gmail.com>
2023-04-13 16:59:28 +0330

    Major changes in Building
    - type and cost (now buildingMaterials) are now non-static
    - added workerCount and effectOnPopularity
    - add decreaseHitpoint method

--- src/models/Building.java
+++ src/models/Building.java
@@ -1,20 +1,49 @@
 package models;
 
-import java.util.ArrayList;
-
 public abstract class Building {
   private String name;
+  private BuildingType type;
+  private MaterialInstance[] buildingMaterials;
   private int hitpoint;
+  private int workerCount = 0;
+  private int effectOnPopularity = 0;
+  private Unit unit;
+
+  public Building(String name, BuildingType type, MaterialInstance[] buildingMaterials, int initialHitpoint,
+      int workerCount, int effectOnPopularity) {
+    this.name = name;
+    this.type = type;
+    this.buildingMaterials = buildingMaterials;
+    this.hitpoint = initialHitpoint;
+    this.workerCount = workerCount;
+    this.effectOnPopularity = effectOnPopularity;
+  }
 
   String getName() {
     return name;
+  }
+
+  public BuildingType getType() {
+    return type;
+  }
+
+  public MaterialInstance[] getBuildingMaterials() {
+    return buildingMaterials;
   }
 
   public int getHitpoint() {
     return hitpoint;
   }
 
-  public static BuildingType type;
+  public int getWorkerCount() {
+    return workerCount;
+  }
 
-  public static ArrayList<MaterialInstance> cost = new ArrayList<>();
+  public int getEffectOnPopularity() {
+    return effectOnPopularity;
+  }
+
+  public void decreaseHitpoint(int value) {
+    hitpoint -= value;
+  }
 }
