6cb5a6cb98ace3058de9d83e9bf01c576210465f
Hosein Khansari <hrk.x11@gmail.com>
2023-04-13 17:07:21 +0330

    Add InventoryBuilding model

---
+++ src/models/buildingsv2/InventoryBuilding.java
@@ -0,0 +1,32 @@
+package models.buildingsv2;
+
+import java.util.ArrayList;
+
+import models.Building;
+import models.BuildingType;
+import models.MaterialInstance;
+
+public class InventoryBuilding<T> extends Building {
+  private int capacity;
+  private ArrayList<T> items;
+
+  public InventoryBuilding(String name, BuildingType type, MaterialInstance[] buildingMaterials, int initialHitpoint,
+      int workerCount, int effectOnPopularity, int capacity) {
+    super(name, type, buildingMaterials, initialHitpoint, workerCount, effectOnPopularity);
+    this.capacity = capacity;
+  }
+
+  public int getCapacity() {
+    return capacity;
+  }
+
+  public void add(T item) {
+    if (items.size() < capacity) {
+      items.add(item);
+    }
+  }
+
+  public int getCount() {
+    return items.size();
+  }
+}
