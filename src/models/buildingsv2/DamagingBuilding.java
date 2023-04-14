c56cc1e97b04cf1eaa2b965f624195414b9b4fa6
Hosein Khansari <hrk.x11@gmail.com>
2023-04-14 13:49:19 +0330

    Add some building classes and types

---
+++ src/models/buildingsv2/DamagingBuilding.java
@@ -0,0 +1,19 @@
+package models.buildingsv2;
+
+import models.Building;
+import models.BuildingType;
+import models.MaterialInstance;
+
+public class DamagingBuilding extends Building {
+  private int damage;
+
+  public DamagingBuilding(String name, BuildingType type, MaterialInstance[] buildingMaterials, int initialHitpoint,
+      int workerCount, int effectOnPopularity, int damage) {
+    super(name, type, buildingMaterials, initialHitpoint, workerCount, effectOnPopularity);
+    this.damage = damage;
+  }
+
+  public int getDamage() {
+    return damage;
+  }
+}
---
+++ src/models/buildingsv2/ProductionBuilding.java
@@ -0,0 +1,36 @@
+package models.buildingsv2;
+
+import models.Building;
+import models.BuildingType;
+import models.MaterialInstance;
+
+public class ProductionBuilding<T> extends Building {
+  private int rate;
+  private MaterialInstance neededMaterial;
+  private T product;
+
+  public ProductionBuilding(String name, BuildingType type, MaterialInstance[] buildingMaterials, int initialHitpoint,
+      int workerCount, int effectOnPopularity, int rate, T product) {
+    super(name, type, buildingMaterials, initialHitpoint, workerCount, effectOnPopularity);
+    this.rate = rate;
+    this.product = product;
+  }
+
+  public ProductionBuilding(String name, BuildingType type, MaterialInstance[] buildingMaterials, int initialHitpoint,
+      int workerCount, int effectOnPopularity, int rate, T product, MaterialInstance neededMaterial) {
+    super(name, type, buildingMaterials, initialHitpoint, workerCount, effectOnPopularity);
+    this.neededMaterial = neededMaterial;
+  }
+
+  public int getRate() {
+    return rate;
+  }
+
+  public T getProduct() {
+    return product;
+  }
+
+  public boolean needsMaterial() {
+    return neededMaterial != null;
+  }
+}
