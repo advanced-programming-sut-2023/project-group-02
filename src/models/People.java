08012b67ec7b0a8e82d00aa68f433180f5450403
Danial <Danielparnian2@gmail.com>
2023-04-14 22:16:45 +0430

    little changes for People

--- src/models/People.java
+++ src/models/People.java
@@ -4,6 +4,27 @@ import models.buildings.PlainBuilding;
 
 public class People {
     private PlainBuilding livingPlace;
-    // TODO add working place
+    private String workingPlace;
+    boolean hasJob = false;
 
+    public People(PlainBuilding livingPlace) {
+        this.livingPlace = livingPlace;
+    }
+
+    public void recruit(String workingPlace) {
+        hasJob = true;
+        this.workingPlace = workingPlace;
+    }
+
+    public PlainBuilding getLivingPlace() {
+        return livingPlace;
+    }
+
+    public String getWorkingPlace() {
+        return workingPlace;
+    }
+
+    public boolean isHasJob() {
+        return hasJob;
+    }
 }
