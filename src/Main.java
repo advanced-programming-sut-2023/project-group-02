553ab74be920c246699fec47c7675ae184660db3
NimaNM7 <moazzen24@gmail.com>
2023-04-14 23:25:00 +0430

    add main

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import utils.Randoms;
+import utils.Validation;
+import view.SignupMenu;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        SignupMenu.run(scanner);
+    }
+}
