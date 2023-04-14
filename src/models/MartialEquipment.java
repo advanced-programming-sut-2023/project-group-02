ec0936e1150670ea134ca1bf81207b4b3a4b9bf2
Danial <Danielparnian2@gmail.com>
2023-04-14 20:35:28 +0430

    martial equipment added

---
+++ src/models/MartialEquipment.java
@@ -0,0 +1,19 @@
+package models;
+
+public enum MartialEquipment {
+    // made in Poleturner using wood:
+    SPEAR,
+    PIKE,
+    // made in Fletcher using wood:
+    BOW,
+    CROOSBOW,
+    // made in Armourer:
+    METAL_ARMOUR,
+    // made in Diary Farm:
+    LEATHER_ARMOUR,
+    // made in Black Smith using Iron:
+    MACE,
+    SWORD,
+    // made in Stable:
+    HORSE;
+}
