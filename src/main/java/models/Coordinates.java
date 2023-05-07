e973f2d279720620a12755e3ef559ff666211fad
Hosein Khansari <hrk.x11@gmail.com>
2023-05-07 21:56:25 +0330

    Add PathFinder util

---
+++ src/main/java/models/Coordinates.java
@@ -0,0 +1,40 @@
+package models;
+
+public class Coordinates {
+    public final int x, y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Coordinates other = (Coordinates) obj;
+        if (x != other.x)
+            return false;
+        if (y != other.y)
+            return false;
+        return true;
+    }
+
+    public Coordinates getNeighbour(Directions direction) {
+        return switch (direction) {
+            case NORTH -> new Coordinates(x, y - 1);
+            case EAST -> new Coordinates(x + 1, y);
+            case SOUTH -> new Coordinates(x, y + 1);
+            case WEST -> new Coordinates(x - 1, y);
+        };
+    }
+}
---
+++ src/main/java/utils/PathFinder.java
@@ -0,0 +1,79 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+import models.Cell;
+import models.Map;
+import models.Coordinates;
+
+public class PathFinder {
+    private Map map;
+    private final Coordinates startingPoint, endPoint;
+    private LinkedList<Coordinates> path = new LinkedList<>();
+    private HashSet<Coordinates> visitedPoints = new HashSet<>();
+
+    private PathFinder(Map map, int sx, int sy, int ex, int ey) {
+        this.map = map;
+        this.startingPoint = new Coordinates(sx, sy);
+        this.endPoint = new Coordinates(ex, ey);
+        bfs();
+    }
+
+    private void bfs() {
+        visitedPoints.add(startingPoint);
+
+        // create a queue of points
+        LinkedList<LinkedList<Coordinates>> queue = new LinkedList<>();
+
+        // add to path containing the starting point to the queue
+        LinkedList<Coordinates> startingPath = new LinkedList<>();
+        startingPath.add(startingPoint);
+        queue.add(startingPath);
+
+        while (queue.size() > 0) {
+            LinkedList<Coordinates> path = queue.get(0);
+            queue.poll();
+            Coordinates point = path.get(path.size() - 1);
+
+            if (point.equals(endPoint)) {
+                this.path = path;
+                return;
+            }
+
+            ArrayList<Coordinates> neighbors = getNeighbors(point);
+            for (Coordinates neighbor : neighbors) {
+                Cell cell = map.findCellWithXAndY(neighbor.x, neighbor.y);
+                if (!visitedPoints.contains(neighbor) && !cell.isOccupied()) {
+                    visitedPoints.add(neighbor);
+                    LinkedList<Coordinates> newPath = new LinkedList<>(path);
+                    newPath.add(neighbor);
+                    queue.add(newPath);
+                }
+            }
+        }
+    }
+
+    private ArrayList<Coordinates> getNeighbors(Coordinates point) {
+        ArrayList<Coordinates> neighbors = new ArrayList<>();
+        if (point.x > 0) {
+            neighbors.add(new Coordinates(point.x - 1, point.y));
+        }
+        if (point.x < map.getWidth() - 1) {
+            neighbors.add(new Coordinates(point.x + 1, point.y));
+        }
+        if (point.y > 0) {
+            neighbors.add(new Coordinates(point.x, point.y - 1));
+        }
+        if (point.y < map.getHeight() - 1) {
+            neighbors.add(new Coordinates(point.x, point.y + 1));
+        }
+        return neighbors;
+    }
+
+    public static LinkedList<Coordinates> getPath(Map map, int sx, int sy, int ex, int ey) {
+        PathFinder bfs = new PathFinder(map, sx, sy, ex, ey);
+        return bfs.path;
+    }
+}
